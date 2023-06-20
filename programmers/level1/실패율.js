3077907719c3ce8f4d4eab9421258c0a626c5918
ericagong <48196721+ericagong@users.noreply.github.com>
2023-06-20 11:53:45 +0900

    실패율 #48 (2차풀이)

--- programmers/level1/실패율.js
+++ programmers/level1/실패율.js
@@ -26,3 +26,20 @@ function solution(N, stages) {
     })
     .map((item) => item[1]);
 }
+
+function solution2(N, stages) {
+  const result = [];
+  for (let i = 1; i <= N; i++) {
+    const reach = stages.filter((stage) => stage >= i).length;
+    const curr = stages.filter((stage) => stage === i).length;
+    const calc = reach === 0 ? 0 : curr / reach;
+    result.push([i, calc]); // [스테이지 번호, 실패율]
+  }
+
+  return result
+    .sort((a, b) => {
+      if (a[1] !== b[1]) return b[1] - a[1]; // 실패율 내림차순
+      return a[0] - b[0]; // 스테이지 번호 오름차순
+    })
+    .map((elem) => elem[0]);
+}
