9f8b3dfe01abe25ed50d23c6116b3f9b3900f3a1
ericagong <48196721+ericagong@users.noreply.github.com>
2023-06-23 15:51:16 +0900

    6/23 압축 #54

---
+++ programmers/level2/압축.js
@@ -0,0 +1,41 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=javascript
+
+function solution(msg) {
+  let result = [];
+
+  let dict = new Map();
+
+  // 뒤에서부터 효율적으로 인덱스 찾기 위해, dict의 key 중 가장 긴 길이 별도로 저장
+  let max_len = 1;
+  let indexes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < indexes.length; i++) {
+    dict.set(indexes[i], i + 1);
+  }
+
+  while (msg.length > 0) {
+    let w = "";
+
+    // w 찾기
+    for (let i = max_len; i > 0; i--) {
+      w = msg.slice(0, i);
+      if (dict.has(w)) {
+        // 	w에 해당하는 색인 번호 출력
+        result.push(dict.get(w));
+        break;
+      }
+    }
+
+    // 입력에서 w 제거
+    msg = msg.slice(w.length);
+
+    // wc 사전 추가
+    if (msg.length !== 0) {
+      let key = w + msg[0];
+      dict.set(key, dict.size + 1);
+
+      max_len = Math.max(max_len, key.length); // 최장 길이 반영
+    }
+  }
+
+  return result;
+}
