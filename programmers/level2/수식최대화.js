182e60e64e82a5125b6d6d556a2d6fd79d74b62f
ericagong <48196721+ericagong@users.noreply.github.com>
2023-07-05 17:08:52 +0900

    7/5 수식 최대화 2차 풀이(with eval, 재귀함수) #81

--- programmers/level2/수식최대화.js
+++ programmers/level2/수식최대화.js
@@ -64,3 +64,44 @@ function solution(expression) {
 
   return max_result;
 }
+
+// 풀이2: eval 사용
+
+// function solution(expression) {
+//   const splitted = expression.split(/([\*\+-])/g);
+
+//   const solve = (precedence, left = 0, right = splitted.length) => {
+//     if (left + 1 === right) {
+//       return eval(splitted[left]);
+//     }
+
+//     for (const operator of precedence) {
+//       for (let i = right - 2; i > left; i -= 2) {
+//         if (splitted[i] === operator) {
+//           return eval(
+//             `${solve(precedence, left, i)}${operator}${solve(
+//               precedence,
+//               i + 1,
+//               right
+//             )}`
+//           );
+//         }
+//       }
+//     }
+
+//     return Number.POSITIVE_INIFINITY;
+//   };
+
+//   return Math.max(
+//     ...[
+//       ["*", "+", "-"],
+//       ["*", "-", "+"],
+//       ["+", "*", "-"],
+//       ["+", "-", "*"],
+//       ["-", "*", "+"],
+//       ["-", "+", "*"],
+//     ]
+//       .map((precedence) => solve(precedence))
+//       .map(Math.abs)
+//   );
+// }
