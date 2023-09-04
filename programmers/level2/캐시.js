c33ce901398b5b6e86b7a1b50db057eef1161ec6
ericagong <48196721+ericagong@users.noreply.github.com>
2023-09-04 10:51:57 +0900

    resolved: 9/4 캐시 - Map을 이용해 시간복잡도 개선

--- programmers/level2/캐시.js
+++ programmers/level2/캐시.js
@@ -1,6 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/17680
 
-function solution(cacheSize, cities) {
+function solutionWithQueue(cacheSize, cities) {
   const MISS = 5;
   const HIT = 1;
 
@@ -30,4 +30,30 @@ function solution(cacheSize, cities) {
   });
 
   return answer;
+}
+
+function solutionWithMap(cacheSize, cities) {
+  if (cacheSize === 0) return cities.length * 5;
+
+  cities = cities.map((city) => city.toLowerCase());
+
+  let time = 0;
+  const cache = new Map();
+
+  cities.forEach((city, t) => {
+    if (cache.has(city)) {
+      time += 1;
+      cache.delete(city);
+      cache.set(city, t);
+    } else {
+      time += 5;
+      if (cache.size === cacheSize) {
+        const target = cache.keys().next().value;
+        cache.delete(target);
+      }
+      cache.set(city, t);
+    }
+  });
+
+  return time;
 }
