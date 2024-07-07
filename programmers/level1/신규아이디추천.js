4f719c4129e035222f3800179c39d33853406534
ericagong <48196721+ericagong@users.noreply.github.com>
2024-07-08 06:57:23 +0900

    7/8 신규 아이디 추천(18min)

---
+++ programmers/level1/신규아이디추천.js
@@ -0,0 +1,19 @@
+function solution1(new_id) {
+  let id = new_id
+    .toLowerCase()
+    .replaceAll(/[^\da-z\-_.]/g, "")
+    .replaceAll(/[.]{2,}/g, ".");
+
+  if (id.startsWith(".")) id = id.slice(1);
+  if (id.endsWith(".")) id = id.slice(0, id.length - 1);
+
+  if (id.length === 0) id = "a";
+  if (id.length >= 16) {
+    id = id.slice(0, 15);
+    if (id.endsWith(".")) id = id.slice(0, id.length - 1);
+  }
+  if (id.length <= 2) id = id.padEnd(3, id[id.length - 1]);
+
+  // console.log(id)
+  return id;
+}
