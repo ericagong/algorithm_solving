55f14527e475c1844d94d7162722b3248f6d3410
Erica <48196721+ericagong@users.noreply.github.com>
2024-11-19 09:38:06 +0900

    이코테: 구현(시뮬레이션) - 게임 개발(11/19)
    0. TC = O(N * M) <= 50^2
    1. 배운 점
    - 시뮬레이션 유형 풀이 순서: 손구현 -> 코드 옮기기 (이 때, 유의할 점 반드시 주황색 checkbox로 그린 뒤, 구현하며 체크)
    - 이동 함수는 dx, dy 리스트 패턴으로 풀이
    - 방향 전환은 nextIdx < 0 인 예외 처리 필수 - 삼항 연산자 사용 권장 예_ nd = cd - 2 < 0 ? cd + 2 : cd - 2
    - while문 사용 시, 무한루프에 주의 - 탈출 flag 사용 + break 필수 예_ checked
    - 풀이방식 한 번 정하고 난 뒤에는 다른 풀이법이 있을지 고민하지 말고 바바리안처럼 바로 구현하기!!

---
+++ 이것이취업을위한코딩테스트다/구현/게임개발.js
@@ -0,0 +1,66 @@
+// test input
+// 4 4
+// 1 1 0
+// 1 1 1 1
+// 1 0 0 1
+// 1 1 0 1
+// 1 1 1 1
+
+// test output
+// 3
+
+const fs = require('fs');
+const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n'); // trim 꼭 넣기
+
+const [N, M] = inputs[0].split(' ').map(Number);
+const [A, B, d] = inputs[1].split(' ').map(Number);
+const m = inputs.slice(2).map((items) => items.split(' ').map(Number));
+
+// console.log(N, M, A, B, d);
+// console.log(m);
+
+// 북 동 남 서
+// 0 1 2 3
+const dx = [-1, 0, 1, 0];
+const dy = [0, 1, 0, -1];
+let cx = A;
+let cy = B;
+let cd = d;
+let checked = 0;
+while (checked <= 4) {
+  if (checked === 4) {
+    let bd = cd - 2 < 0 ? cd + 2 : cd - 2;
+    const nx = cx + dx[bd];
+    const ny = cy + dy[bd];
+    if (nx < 0 || ny < 0 || nx >= N || ny >= M || m[nx][ny] === 1) break;
+    else {
+      cd = cd;
+      m[nx][ny] = 2;
+      cx = nx;
+      cy = ny;
+      checked = 0;
+    }
+  }
+  let nd = cd - 1 < 0 ? 3 : cd - 1;
+  checked += 1;
+  const nx = cx + dx[nd];
+  const ny = cy + dy[nd];
+  if (nx >= 0 && ny >= 0 && nx < N && ny < M && m[nx][ny] === 0) {
+    cd = nd;
+    m[nx][ny] = 2;
+    cx = nx;
+    cy = ny;
+    checked = 0;
+  } else cd = nd;
+}
+
+// console.log(m);
+
+let count = 0;
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (m[i][j] === 2) count += 1;
+  }
+}
+
+console.log(count);
