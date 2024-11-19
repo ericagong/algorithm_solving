3f0f377e89b289d6c8428c997b037753f4d1959a
Erica <48196721+ericagong@users.noreply.github.com>
2024-11-19 19:14:57 +0900

    programmers: LV.1 키패드 누르기(11/19)
    0. TC : O(n)
    1. 배운 점
    - if문 작성시, 항상 논리적 방향과 부등호 방향/조건 일치하는지 더블체크하기
    - 초기화 시, 요구되는 기능에 따라 Map, Set, Array, Object 등 적합한 데이터 방향 미리 정하기
    - 구조 분해 할당 시, let const 키워드는 전체 변수에 동일하게 적용됨 (반드시 let,const 쓰는 습관 -> 미부여시 의도하지 않은 방식으로 JSE가 문장 단위 해석할 여지 있음)
    - 빠른 구현이 중요한 코테환경에서는 규칙 찾기 << 하드 코딩 일 때가 많음을 명심하기

--- programmers/level1/키패드누르기.js
+++ programmers/level1/키패드누르기.js
@@ -5,7 +5,7 @@ function solution(numbers, hand) {
     [1, [0, 0]],
     [4, [1, 0]],
     [7, [2, 0]],
-    ["*", [3, 0]],
+    ['*', [3, 0]],
   ]);
   const middle = new Map([
     [2, [0, 1]],
@@ -17,19 +17,19 @@ function solution(numbers, hand) {
     [3, [0, 2]],
     [6, [1, 2]],
     [9, [2, 2]],
-    ["#", [3, 2]],
+    ['#', [3, 2]],
   ]);
 
-  let cl = left.get("*");
-  let cr = right.get("#");
-  let result = "";
+  let cl = left.get('*');
+  let cr = right.get('#');
+  let result = '';
 
   for (num of numbers) {
     if (left.has(num)) {
-      result += "L";
+      result += 'L';
       cl = left.get(num);
     } else if (right.has(num)) {
-      result += "R";
+      result += 'R';
       cr = right.get(num);
     } else {
       const [tx, ty] = middle.get(num);
@@ -38,21 +38,91 @@ function solution(numbers, hand) {
       let dl = Math.abs(lx - tx) + Math.abs(ly - ty);
       let dr = Math.abs(rx - tx) + Math.abs(ry - ty);
       if (dl < dr) {
-        result += "L";
+        result += 'L';
         cl = [tx, ty];
       } else if (dr < dl) {
-        result += "R";
+        result += 'R';
         cr = [tx, ty];
       } else {
-        if (hand === "left") {
-          result += "L";
+        if (hand === 'left') {
+          result += 'L';
           cl = [tx, ty];
         } else {
-          result += "R";
+          result += 'R';
           cr = [tx, ty];
         }
       }
     }
   }
+  return result;
+}
+
+function solution2(numbers, hand) {
+  const ps = new Map([
+    [1, [0, 0]],
+    [2, [0, 1]],
+    [3, [0, 2]],
+    [4, [1, 0]],
+    [5, [1, 1]],
+    [6, [1, 2]],
+    [7, [2, 0]],
+    [8, [2, 1]],
+    [9, [2, 2]],
+    ['*', [3, 0]],
+    [0, [3, 1]],
+    ['#', [3, 2]],
+  ]);
+
+  const lefts = new Set([1, 4, 7]);
+  const rights = new Set([3, 6, 9]);
+
+  let cl = '*';
+  let cr = '#';
+  let [clx, cly] = ps.get(cl);
+  let [crx, cry] = ps.get(cr);
+
+  let result = '';
+  const preferred = hand === 'left' ? 'L' : 'R';
+
+  numbers.forEach((num) => {
+    if (lefts.has(num)) {
+      cl = num;
+      clx = ps.get(cl)[0];
+      cly = ps.get(cl)[1];
+      result += 'L';
+    } else if (rights.has(num)) {
+      cr = num;
+      crx = ps.get(cr)[0];
+      cry = ps.get(cr)[1];
+      result += 'R';
+    } else {
+      const [tx, ty] = ps.get(num);
+      const dl = Math.abs(clx - tx) + Math.abs(cly - ty);
+      const dr = Math.abs(crx - tx) + Math.abs(cry - ty);
+      if (dr > dl) {
+        cl = num;
+        clx = ps.get(cl)[0];
+        cly = ps.get(cl)[1];
+        result += 'L';
+      } else if (dl > dr) {
+        cr = num;
+        crx = ps.get(cr)[0];
+        cry = ps.get(cr)[1];
+        result += 'R';
+      } else {
+        if (preferred === 'L') {
+          cl = num;
+          clx = ps.get(cl)[0];
+          cly = ps.get(cl)[1];
+          result += 'L';
+        } else {
+          cr = num;
+          crx = ps.get(cr)[0];
+          cry = ps.get(cr)[1];
+          result += 'R';
+        }
+      }
+    }
+  });
   return result;
 }
