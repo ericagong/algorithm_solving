536e8c0378b20edb4ffc04397cb084176b3e5c52
Erica <48196721+ericagong@users.noreply.github.com>
2024-11-23 16:38:12 +0900

    programmers: level1 신고 결과 받기(11/23)
    0. TC
    - sol1 = O(N + M) , M = 중복 제거한 뒤 reports 길이
    - sol2 = O(N^2), 최악의 경우 모두가 서로를 신고할 수 있음
    
    1. 배운점
    - 코테 환경이든 실무 환경이든, 중복은 맨 처음 없애고 시작해야함: [...new Set(duplicatedData)] 사용
    - map 선언 후, 값 넣으면서 동시에 초기화하는 방법: map.set(k, map.get(k) + 1 || 1)
    - programmers에는 24.6 JS 도입된 Set.prototype.intersection, difference, union 등 아직 적용 불가

--- programmers/level1/신고결과받기.js
+++ programmers/level1/신고결과받기.js
@@ -1,6 +1,34 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/92334
 
+/** O(N+M): M = 중복 제거한 뒤, reports 길이
+ * 코테환경에서는 중복을 먼저 제거하고 푸는 방식을 추천 */
 function solution1(id_list, report, k) {
+  // 유저 A -> B로의 신고가 한 번만 신고 가능하므로 Set으로 중복 제거
+  let reports = [...new Set(report)].map((a) => {
+    return a.split(' ');
+  });
+
+  let reported = new Map(); // key = 신고 당한 유저 value = 신고당한 횟수
+  for (const report of reports) {
+    reported.set(report[1], reported.get(report[1]) + 1 || 1);
+  }
+
+  let reporting = new Map(); // key = 신고한 유저 value = 신고한 횟수
+  for (const report of reports) {
+    if (reported.get(report[1]) >= k) {
+      reporting.set(report[0], reporting.get(report[0]) + 1 || 1);
+    }
+  }
+
+  // 유저가 받은 메일 수 반환
+  let answer = id_list.map((a) => reporting.get(a) || 0);
+  return answer;
+}
+
+/** 최악의 경우, 신고 당한 유저에 신고한 유저가 모두 포함될 수 있음
+ *  O(N^2) 되므로 비권장 */
+
+function solution2(id_list, report, k) {
   const reported = new Map();
   const reporting = new Map();
   id_list.forEach((user) => {
@@ -27,27 +55,4 @@ function solution1(id_list, report, k) {
   // console.log(result)
 
   return result;
-}
-
-function solution2(id_list, report, k) {
-  // 유저 A -> B로의 신고가 한 번만 신고 가능하므로 Set으로 중복 제거
-  let reports = [...new Set(report)].map((a) => {
-    return a.split(' ');
-  });
-
-  let reported = new Map(); // key = 신고 당한 유저 value = 신고당한 횟수
-  for (const report of reports) {
-    reported.set(report[1], reported.get(report[1]) + 1 || 1);
-  }
-
-  let reporting = new Map(); // key = 신고한 유저 value = 신고한 횟수
-  for (const report of reports) {
-    if (reported.get(report[1]) >= k) {
-      reporting.set(report[0], reporting.get(report[0]) + 1 || 1);
-    }
-  }
-
-  // 유저가 받은 메일 수 반환
-  let answer = id_list.map((a) => reporting.get(a) || 0);
-  return answer;
 }
