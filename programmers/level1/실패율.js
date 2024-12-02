ef9ef4d686ee698b71b2753bc4d32025658f06b9
Erica <48196721+ericagong@users.noreply.github.com>
2024-12-02 11:19:28 +0900

    programmers: level1 실패율(12/2)
    0. TC = O(Max(N^2, M)) <= Max(5^2 * 10^4, 2 * 10^5)
    
    1. 배운점
    - 배열 초기화 시, index가 데이터와 매칭되는지 아니면 index+-1로 처리해야하는지 메모하고 반영하는 습관 들이기
    - 전체 누적에 대해 차감하기 위해서는, 전체 합 구한 뒤, 앞에서부터 차감해나가야함
    - splice vs slice: 신규 배열 반환 vs 원본 배열 변경

--- programmers/level1/실패율.js
+++ programmers/level1/실패율.js
@@ -1,6 +1,44 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42889
 
 function solution1(N, stages) {
+  const totalPlayers = stages.length;
+  const players = Array.from({ length: N + 1 }).fill(0);
+
+  stages.forEach((stg) => (players[stg - 1] += 1));
+
+  let accum = totalPlayers;
+  const rates = players.map((curr, idx) => {
+    const stg = idx + 1;
+    if (curr === 0) return [stg, 0];
+    const rate = curr / accum;
+    accum -= curr;
+    return [stg, rate];
+  });
+
+  return rates
+    .slice(0, N)
+    .sort((a, b) => b[1] - a[1])
+    .map((item) => item[0]);
+}
+
+function solution2(N, stages) {
+  const result = [];
+  for (let i = 1; i <= N; i++) {
+    const reach = stages.filter((stage) => stage >= i).length;
+    const curr = stages.filter((stage) => stage === i).length;
+    const calc = reach === 0 ? 0 : curr / reach;
+    result.push([i, calc]); // [스테이지 번호, 실패율]
+  }
+
+  return result
+    .sort((a, b) => {
+      if (a[1] !== b[1]) return b[1] - a[1]; // 실패율 내림차순
+      return a[0] - b[0]; // 스테이지 번호 오름차순
+    })
+    .map((elem) => elem[0]);
+}
+
+function solution3(N, stages) {
   let curPlayers = new Array(N).fill(0);
 
   for (let stage of stages) {
@@ -25,21 +63,4 @@ function solution1(N, stages) {
       return a[1] - b[1]; // 스테이지 번호 오름차순
     })
     .map((item) => item[1]);
-}
-
-function solution2(N, stages) {
-  const result = [];
-  for (let i = 1; i <= N; i++) {
-    const reach = stages.filter((stage) => stage >= i).length;
-    const curr = stages.filter((stage) => stage === i).length;
-    const calc = reach === 0 ? 0 : curr / reach;
-    result.push([i, calc]); // [스테이지 번호, 실패율]
-  }
-
-  return result
-    .sort((a, b) => {
-      if (a[1] !== b[1]) return b[1] - a[1]; // 실패율 내림차순
-      return a[0] - b[0]; // 스테이지 번호 오름차순
-    })
-    .map((elem) => elem[0]);
 }
