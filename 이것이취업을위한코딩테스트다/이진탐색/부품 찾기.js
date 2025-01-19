98963f57e0d8a82bf0a54c449e479686805d0926
Erica <48196721+ericagong@users.noreply.github.com>
2025-01-19 15:17:56 +0900

    이코테: [복습] 부품찾기 (1/19)
    **0. Time Complexity**
    
    - O(Max(N, M))
        - set 자료 삽입 O(1) * N
        - set 기반 탐색 O(1) * M
    
    **1. 배운 점 3가지**
    
    - 이진 탐색은 `while(s ≤ e)` 반복문으로 구현
    - 이진 탐색 시, `arr[m] < t → s = m + 1`, `arr[m] > t → e = m - 1` 암기
    - 이진 탐색 실패 시 `return -1`

--- 이것이취업을위한코딩테스트다/이진탐색/부품 찾기.js
+++ 이것이취업을위한코딩테스트다/이진탐색/부품 찾기.js
@@ -24,3 +24,21 @@ const result = orders.map((item) => binarySearch(tools, item, 0, N - 1));
 // console.log(result)
 
 console.log(result.join(' '));
+
+// 집합 자료형 기반 풀이
+
+const fs2 = require('fs');
+const inputs2 = fs2.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N2 = Number(inputs2[0]);
+const items = new Set(inputs2[1].split(' ').map(Number));
+const M2 = Number(inputs2[2]);
+const orders2 = inputs2[3].split(' ').map(Number);
+// console.log(N2, items, M2, orders2)
+
+const result2 = orders2.map((item) => {
+  if (items.has(item)) return 'yes';
+  else return 'no';
+});
+
+console.log(result2.join(' '));
