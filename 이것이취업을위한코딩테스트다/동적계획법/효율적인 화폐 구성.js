19662b7f047d13ae26f8ac93e53719c09d00ba62
Erica <48196721+ericagong@users.noreply.github.com>
2025-01-23 16:21:57 +0900

    이코테: [복습] 동적계획법 - 효율적인 화폐구성(1/23)
    **0. Time Complexity**
    
    - Bottom-up 기반 DP
        - O(N*M)
            - N에 대해 M번 최소값 업데이트
            - 1 ≤ N ≤ 10^2, 1 ≤ M ≤ 10^4, O(NM) ≤ 10^6 < 10^9
    
    **1. 배운 점 3가지**
    
    - 구현 순서 `ㅁ숫자` 형식으로 순서대로 표시 → 단계별 디버깅 확인하며 순차적 풀이
    - 배열 접근 전, 항상 해당 인덱스 접근 가능 여부 선확인 : `인덱스 가드`
        - espc. 숫자 계산 결과 NAN → 배열 밖 인덱스 접근 의심
    - `console.log(평가식)` 작성 가능 예_console.log(arr[m] === Infinity ? -1 : arr[m])

--- 이것이취업을위한코딩테스트다/동적계획법/효율적인 화폐 구성.js
+++ 이것이취업을위한코딩테스트다/동적계획법/효율적인 화폐 구성.js
@@ -5,19 +5,15 @@ const [N, M] = inputs[0].split(' ').map(Number);
 const coins = Array.from({ length: N }, (_, i) => Number(inputs[i + 1]));
 // console.log(N, M, coins)
 
-const dp = Array(M + 1).fill(0);
-coins.forEach((c) => (dp[c] = 1));
+const dp = Array(M + 1).fill(Infinity);
+coins.forEach((coin) => (dp[coin] = 1));
 // console.log(dp)
 
 for (let i = 1; i <= M; i++) {
-  if (dp[i] !== 0) continue;
-  let min = Infinity;
-  coins.forEach((c) => {
-    if (i - c > 0 && dp[i - c] !== -1) min = Math.min(min, dp[i - c] + 1);
-  });
-
-  dp[i] = min === Infinity ? -1 : min;
+  for (const coin of coins) {
+    if (i - coin > 0) dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+  }
 }
 
 // console.log(dp)
-console.log(dp[M]);
+console.log(dp[M] === Infinity ? -1 : dp[M]);
