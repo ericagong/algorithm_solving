a0050f7ff22d386fa3e31856de11bb2c8a9aad0e
Erica <48196721+ericagong@users.noreply.github.com>
2025-01-24 17:29:25 +0900

    programmers: [lv1] 비밀지도(1/24)
    1. 배운점
    - Number.prototype.toString(base) -> base 진법으로 10진수 Number 변환한 문자열 반환
      - base default = 10
    - String.prototype.padStart(num, '문자열') (cf.padEnd) -> String이 num 길이가 될 때까지 문자열 채움
      - 예외 케이스 알아두기!
    -  X를 N진법 직접 변환: (1) while(X < N) 동안 나머지 배열 저장 + 마지막 나머지 0 아니면 추가 저장 (2) 나머지 배열 reverse -> join 해 문자열로 반환

--- programmers/level1/비밀지도.js
+++ programmers/level1/비밀지도.js
@@ -1,7 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/17681
 
-// solution : 손이 가장 빠른 하드코딩 풀이
-function solution1(n, arr1, arr2) {
+function solution(n, arr1, arr2) {
   arr1 = arr1.map((i) => i.toString(2).padStart(n, '0'));
   arr2 = arr2.map((i) => i.toString(2).padStart(n, '0'));
 
@@ -18,43 +17,14 @@ function solution1(n, arr1, arr2) {
   return answer;
 }
 
-function getDecimal(n, num) {
-  let res = [];
-  while (num >= 2) {
-    r = num % 2;
-    num = parseInt(num / 2);
-    res.push(r);
-  }
-  if (num !== 0) res.push(num);
-  res.reverse();
-  return res.join('').padStart(n, 0);
-}
-
-function solution2(n, arr1, arr2) {
-  let answer = [];
-  let map1 = [];
-  let map2 = [];
-
-  for (const num of arr1) {
-    // map1.push(getDecimal(n, num))
-    // Number.toString(base) 함수로 바로 특정 진법의 문자열 얻을 수 있음
-    map1.push(num.toString(2).padStart(n, 0));
-  }
-  for (const num of arr2) {
-    // map2.push(getDecimal(n, num))
-    map2.push(num.toString(2).padStart(n, 0));
-  }
-
-  for (let i = 0; i < n; i++) {
-    temp = '';
-    for (let j = 0; j < n; j++) {
-      // 숫자가 아닌 문자열로 확인해야함에 주의하기
-      if (map1[i][j] === '0' && map2[i][j] === '0') {
-        temp += ' ';
-      } else temp += '#';
-    }
-    answer[i] = temp;
-  }
-
-  return answer;
-}
+// function getDecimal(n, num) {
+//   let res = [];
+//   while (num >= 2) {
+//     r = num % 2;
+//     num = parseInt(num / 2);
+//     res.push(r);
+//   }
+//   if (num !== 0) res.push(num);
+//   res.reverse();
+//   return res.join('').padStart(n, 0);
+// }
