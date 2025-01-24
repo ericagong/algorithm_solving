b5d653f14748266fdf97ad3bc50799ae4c3f5b31
Erica <48196721+ericagong@users.noreply.github.com>
2025-01-24 17:58:39 +0900

    programmers: [lv1] 신규 아이디 추천(1/24)
    1. 배운점
    - String.prototype.replace(/제거정규식/g, '') -> 특정 정규식 형태 모두 제거
    - /^시작/ /끝$/ /[^불포함]
    - String.prototype.replace()가 String.prototype.padEnd() 처럼 쓰는 방법 확인하기

--- programmers/level1/신규아이디추천.js
+++ programmers/level1/신규아이디추천.js
@@ -1,20 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/72410
 
 function solution1(new_id) {
-  new_id = new_id
-    .toLowerCase()
-    .replace(/[^\w-_.]/g, '')
-    .replace(/\.{2,}/g, '.')
-    .replace(/^\.|\.$/g, '');
-
-  if (new_id.length === 0) new_id = 'a';
-  if (new_id.length >= 16) new_id = new_id.slice(0, 15).replace(/\.$/, '');
-  if (new_id.length <= 2) new_id = new_id.padEnd(3, new_id[new_id.length - 1]);
-
-  return new_id;
-}
-
-function solution2(new_id) {
   return new_id
     .toLowerCase()
     .replace(/[^\w-_.]/g, '')
@@ -26,7 +12,7 @@ function solution2(new_id) {
     .padEnd(3, new_id[new_id.length - 1]);
 }
 
-function solution3(new_id) {
+function solution2(new_id) {
   new_id = new_id
     .toLowerCase()
     .replace(/[^\w-_.]/g, '')
