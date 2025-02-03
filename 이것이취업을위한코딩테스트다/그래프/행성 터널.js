39bdb701705e2d8a5b8bed9c4475a131ee94bcb9
Erica <48196721+ericagong@users.noreply.github.com>
2025-02-03 16:38:38 +0900

    이코테: 그래프 - 행성 터널(2/3)
    **0. Time Complexity**
    
    - 모든 간선에 대해 정렬 수행 시 → 간선 개수 = N(N-1)/2 → O(N^2logN^2) > 10^8 → 불가
        - 1 ≤ N ≤ 10^5
    - x, y, z 좌표에 대해 정렬해 각 경우 N-1 개의 간선에 대해 정렬 → 간선 개수 = 3 * (N-1) → O(3Nlog3N) → 가능
    
    **1. 배운 점 3가지**
    
    - 시간복잡도 > 10^8 → `Input값 고려 케이스 가지치기`
        - 예) 간선 전체 완전 탐색 → 고려 **필요한 간선이 무엇인지 사전 선별**
    - 크루스칼 알고리즘 시간 복잡도 = 간선 정렬 = O(MlogM)
    - 크루스칼 알고리즘 순서
        1. pt 자기 자신으로 초기화
        2. edges 정렬
        3. edges 순회하며 → 각 간선에 대해 cycle 존재하지 않는 경우만 union_parent 수행 `*경로 압축 필수`

---
+++ 이것이취업을위한코딩테스트다/그래프/행성 터널.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(inputs[0]);
+const xs = [], ys = [], zs = [];
+
+for (let i = 0; i < N; i++) {
+  const [px, py, pz] = inputs[i + 1].split(' ').map(Number);
+  xs.push([px, i]);
+  ys.push([py, i]);
+  zs.push([pz, i]);
+}
+
+// 좌표 정렬
+xs.sort((a, b) => a[0] - b[0]);
+ys.sort((a, b) => a[0] - b[0]);
+zs.sort((a, b) => a[0] - b[0]);
+
+const pt = Array.from({length: N + 1)}, (_, i) => i);
+const edges = [];
+let resultCost = 0;
+
+// 간선 3 * (N-1)개에 대해서만 수행
+// 간선 비용 추가 (각 좌표별 최소 간선)
+for (let i = 0; i < N - 1; i++) {
+  edges.push([Math.abs(xs[i + 1][0] - xs[i][0]), xs[i][1], xs[i + 1][1]]);
+  edges.push([Math.abs(ys[i + 1][0] - ys[i][0]), ys[i][1], ys[i + 1][1]]);
+  edges.push([Math.abs(zs[i + 1][0] - zs[i][0]), zs[i][1], zs[i + 1][1]]);
+}
+
+// 간선 정렬 (최소 스패닝 트리 Kruskal 알고리즘)
+edges.sort((a, b) => a[0] - b[0]);
+
+// 유니온 파인드 (Find-Union)
+function find_parent(n) {
+  if (pt[n] !== n) pt[n] = find_parent(pt[n]);
+  return pt[n];
+}
+
+// 경로 압축 필수
+function union_parent(a, b) {
+  a = find_parent(a);
+  b = find_parent(b);
+  if (a < b) pt[b] = a;
+  else pt[a] = b;
+}
+
+// 최소 스패닝 트리 구성
+for (let [ec, ea, eb] of edges) {
+  if (find_parent(ea) !== find_parent(eb)) {
+    union_parent(ea, eb);
+    resultCost += ec;
+  }
+}
+
+console.log(resultCost);
