0a2abce3b5145d07600a2cc7d6d03e98282dbcec
Erica <48196721+ericagong@users.noreply.github.com>
2025-02-10 11:52:19 +0900

    programmers: level2 - [1차] 뉴스 클러스터링(2/10)
    **0. Time Complexity**
    
    - O(N+M)
        - N = st1 길이, M = str2 길이
    
    **1. 배운 점 3가지**
    
    - `String.prototype.match() vs RegExp.prototype.test() 구분` 사용: **매칭 결과 필요시 vs 매칭 여부만 t/f 판단** 시
    - `합집합 크기, 교집합 크기` 구하는 함수 암기
        - *for key of new Set([…cnt1.keys(), …cnt2.keys()])*
        - *union += Math.max(cnt1.get(k) || 0 , cnt2.get(k) || 0)*
        - *intersection += Math.min(cnt1.get(k) || 0 , cnt2.get(k) || 0)*
    - 숫자에서 정수부만 파싱 시 → `Math.trunc()` 사용 권장 `parseInt()` 금지
        - String.prototype.parseInt() → 문자열에서 숫자로 타입 변환 + 소수부 없애기 기능
        - 따라서 Number에 사용시 시맨틱에 맞지 않고, 불필요한 형변환 발생
        - JS는 Math.floor, Math.ceil, Math.tunc **모두 소수 첫째자리 수행 고정 →** 다른 소수점에서 조치 원하면 `Math.method(num * 10^N) / 10^N` 처리 필요

--- programmers/level2/뉴스클러스터링.js
+++ programmers/level2/뉴스클러스터링.js
@@ -1,38 +1,35 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/17677
-
 function getCount(str) {
-  const count = new Map();
+  const cnt = new Map();
   for (let i = 0; i < str.length - 1; i++) {
-    let word = str.slice(i, i + 2);
-    if (word.match(/[^a-zA-Z]/g)) continue;
-    word = word.toLowerCase();
-    count.set(word, count.get(word) + 1 || 1);
+    const curr = str.slice(i, i + 2);
+    if (/[^A-Z]/.test(curr)) continue;
+    cnt.set(curr, cnt.get(curr) + 1 || 1);
   }
-  return count;
+  return cnt;
 }
 
 function solution(str1, str2) {
+  str1 = str1.toUpperCase();
+  str2 = str2.toUpperCase();
   const cnt1 = getCount(str1);
   const cnt2 = getCount(str2);
-
-  const words = new Set();
-  for (const k of cnt1.keys()) {
-    words.add(k);
+  let intersection = 0;
+  let union = 0;
+  for (let [k, v1] of cnt1.entries()) {
+    if (!cnt2.has(k)) union += v1;
+    else {
+      const v2 = cnt2.get(k);
+      const minV = Math.min(v1, v2);
+      const maxV = Math.max(v1, v2);
+      intersection += minV;
+      union += maxV;
+    }
   }
-  for (const k of cnt2.keys()) {
-    words.add(k);
+  for (let [k, v2] of cnt2.entries()) {
+    if (cnt1.has(k)) continue;
+    else union += v2;
   }
 
-  let aAndB = 0;
-  let aOrB = 0;
-  words.forEach((word) => {
-    const a = cnt1.get(word) || 0;
-    const b = cnt2.get(word) || 0;
-    aAndB += Math.min(a, b);
-    aOrB += Math.max(a, b);
-  });
-
-  const MUL = 65536;
-  if (aAndB === 0 && aOrB === 0) return 1 * MUL;
-  else return Math.floor((aAndB / aOrB) * MUL);
+  return union === 0 ? 65536 : Math.trunc((intersection / union) * 65536);
 }
