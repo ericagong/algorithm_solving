f89954e935a1f72f895b6b212ae70ea2c27a0378
Erica <48196721+ericagong@users.noreply.github.com>
2025-02-17 17:18:58 +0900

    programmers: level2 - 오픈채팅방(2/17)
    **0. Time Complexity**
    
    - O(N)
        - 주어진 자료 순회
        - 1 < N ≤ 10^5
    
    **1. 배운 점 3가지**
    
    - 배열 내 요소 문자열 형태 반환 필요 시 → `템플릿 리터럴 적극 활용` `${uid}님이 어쩌구저쩌구.`
    - action 타입에 따라 다른 처리 필요 시 → if문 직관적으로 작성(`문제 요구사항 그대로 반영`) = 규칙 찾기 지양!
    - 사전에 전처리 시 input 주는 경우 → new Set(dataArr)로 중복 전처리 권장

--- programmers/level2/오픈채팅방.js
+++ programmers/level2/오픈채팅방.js
@@ -1,55 +1,19 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=javascript
 
-function get_message(action, nickname) {
-  switch (action) {
-    case "Enter":
-      return `${nickname}님이 들어왔습니다.`;
-    case "Leave":
-      return `${nickname}님이 나갔습니다.`;
-  }
-}
-
 function solution(record) {
-  const users = new Map();
-  const actions = [];
+  const uids = new Map();
+  const result = [];
 
   record.forEach((r) => {
-    const [action, id, nickname] = r.split(" ");
-    // Leave 일 때는 nickname 부재
-    if (action === "Enter" || action === "Change") users.set(id, nickname);
-    if (action !== "Change") actions.push([action, id]);
+    const [action, uid, nickname] = r.split(' ');
+    if (action === 'Enter' || action === 'Change') uids.set(uid, nickname);
+    if (action === 'Enter' || action === 'Leave') result.push([action, uid]);
   });
 
-  const result = [];
-  actions.forEach(([action, id]) => {
-    const nickname = users.get(id);
-    result.push(get_message(action, nickname));
+  return result.map((r) => {
+    const [action, uid] = r;
+    const nickname = uids.get(uid);
+    if (action === 'Enter') return `${nickname}님이 들어왔습니다.`;
+    if (action === 'Leave') return `${nickname}님이 나갔습니다.`;
   });
-
-  return result;
-}
-
-function solution2(record) {
-  const nicknames = new Map();
-  const history = [];
-  record.forEach((item) => {
-    const [action, uid, nickname] = item.split(" ");
-    if (!nicknames.has(uid)) nicknames.set(uid, nickname);
-    else {
-      if (action === "Enter") nicknames.set(uid, nickname);
-    }
-    if (action === "Change") nicknames.set(uid, nickname);
-    else history.push([uid, action]);
-  });
-
-  const result = [];
-  const inMsg = `님이 들어왔습니다.`;
-  const outMsg = `님이 나갔습니다.`;
-  history.forEach(([uid, action]) => {
-    const postfix = action === "Enter" ? inMsg : outMsg;
-    const nickname = nicknames.get(uid);
-    result.push(nickname + postfix);
-  });
-
-  return result;
 }
